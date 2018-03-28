05ffa7c1b73acdcc8f320ed3a3bf35077dffc4ca
ken <ken@keywish-robot.com>
2018-03-28 22:03:48 +0800

    init arduino studio blutooth demo apk

---
+++ KeywishBlutooth/app/src/main/java/com/keywish/blutooth/application/MyApplication.java
@@ -0,0 +1,22 @@
+package com.keywish.blutooth.application;
+
+/**
+ * Created by ken on 2018/3/23.
+ */
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+}
